a06de543fa57b812aed85a0a10b60bd13098c849
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-12-04 09:46:04 +0530

    [Rutwik]: Added TicTacToeUC1  File

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe");
+            TicTacToeGame game = new TicTacToeGame();
+            game.CreateGameBoard();
+        }
+    }
+}
---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    public class TicTacToeGame
+    {
+        public char[] BOARD = new char[10];
+        public void CreateGameBoard()
+        {
+            for (int i = 1; i < BOARD.Length; i++)
+            {
+                BOARD[i] = ' ';
+            }
+        }
+    }
+}
